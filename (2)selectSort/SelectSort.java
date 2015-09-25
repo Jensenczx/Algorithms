f989348c95735186a117288d6012d836f20aab44
Jensenczx <jensenczx@gmail.com>
2015-09-25 16:34:25 +0800

    selectsort

---
+++ (2)selectSort/SelectSort.java
@@ -0,0 +1,28 @@
+public class SelectSort{
+	public static void sort(int [] array){
+		int length = array.length;
+		int min=0;
+		int flag=0;
+		for(int i=0; i<length; i++){
+			min = array[i];
+			flag = i;
+			for(int j=i+1; j<length; j++){
+				if(array[j]<min){
+					min = array[j];
+					flag = j;
+				}
+			}
+			if(flag!=i){
+				array[flag]=array[i];
+				array[i]=min;
+			}
+		}
+		
+	}
+	public static void main (String []args){
+		int [] array ={1,9,4,5};
+		sort(array);
+		for(int i: array)
+			System.out.println(i);
+	}
+}
