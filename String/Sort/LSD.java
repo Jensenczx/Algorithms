75d6cd0fcc4eae3796932f4f70af12cf7b79ca00
Jensenczx <jensenczx@gmail.com>
2015-12-16 21:54:38 +0800

    lsd and msd

---
+++ String/Sort/LSD.java
@@ -0,0 +1,21 @@
+public class LSD{
+	public static void sort(String[]a, int w){
+		int N = a.length;
+		int R = 256;
+		String[] aux = new String[N];
+		for(int d=w-1; d>=0; d--){
+			int[] count = new int [R+1];
+			for(int i=0; i<N; i++)
+				count[a[i].charAt(d)+1]++;
+			for(int r=0; r<R; r++)
+				count[r+1]+= count[r];
+
+			for(int i=0; i<N; i++)
+				aux[count[a[i].charAt[d]]++] = a[i];
+
+			for(int j=0; j<N; j++)
+				a[j] = aux[j];
+		}
+
+	}
+}
