a1564f35362597ad2a5b3f54d78dfa260e24472e
Jensenczx <jensenczx@gmail.com>
2015-12-17 14:03:43 +0800

    qucik3 sort and search of the word tree

--- String/Sort/LSD.java
+++ String/Sort/LSD.java
@@ -3,10 +3,13 @@ public class LSD{
 		int N = a.length;
 		int R = 256;
 		String[] aux = new String[N];
+
 		for(int d=w-1; d>=0; d--){
 			int[] count = new int [R+1];
+			
 			for(int i=0; i<N; i++)
 				count[a[i].charAt(d)+1]++;
+
 			for(int r=0; r<R; r++)
 				count[r+1]+= count[r];
 
